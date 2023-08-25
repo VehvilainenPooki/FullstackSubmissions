f1676b4b54de2a4cb8c1db2f0b7953f3f2d02665
VehvilainenPooki <pooki.vehvilainen@helsinki.fi>
2023-08-25 16:13:25 +0300

    Exercise 1.5

--- Parts/part 1/1-5 course information/src/App.jsx
+++ Parts/part 1/1-5 course information/src/App.jsx
@@ -1,7 +1,7 @@
 const Header = (props) => {
   return (
     <div>
-      <h1>{props.course}</h1>
+      <h1>{props.course.name}</h1>
     </div>
   )
 }
@@ -46,27 +46,29 @@ State of a component 14
 Number of exercises 31*/
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
       <Header course={course}/>
-      <Content parts ={parts}/>
-      <Total exercises={parts}/>
+      <Content parts ={course.parts}/>
+      <Total exercises={course.parts}/>
     </div>
   )
 }
