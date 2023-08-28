a6b3e6b9945f0350c9f6210e95460ba03583ddb0
VehvilainenPooki <pooki.vehvilainen@helsinki.fi>
2023-08-28 16:00:39 +0300

    Exercise 1.10 [fixed]

--- Parts/part 1/6-11 unicafe/src/App.jsx
+++ Parts/part 1/6-11 unicafe/src/App.jsx
@@ -13,11 +13,9 @@ const Statistics = ({good, neutral, bad}) => {
   return (
     <div>
       <h1>statistics</h1>
-      good {good}
-      <br></br>
-      neutral {neutral}
-      <br></br>
-      bad {bad}
+      <StatisticLine text={"good"} value={good}/>
+      <StatisticLine text={"neutral"} value={neutral}/>
+      <StatisticLine text={"bad"} value={bad}/>
       <StatisticLine text={"average"} value={<Average good={good} neutral={neutral} bad={bad}/>}/>
       <StatisticLine text={"positive"} value={<Positive good={good} neutral={neutral} bad={bad}/>}/>
     </div>
