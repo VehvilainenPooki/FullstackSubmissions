ad65826422e63d6f900bf09bdff257f914359211
VehvilainenPooki <pooki.vehvilainen@helsinki.fi>
2024-01-25 22:21:29 +0200

    Exercise 2.8

--- Parts/part 2/6-10 The Phonebook/src/App.jsx
+++ Parts/part 2/6-10 The Phonebook/src/App.jsx
@@ -5,12 +5,14 @@ const App = () => {
   const [persons, setPersons] = useState([
     {
       id: 1,
-      name: 'Arto Hellas'
+      name: 'Arto Hellas',
+      number: '040-904-145'
     }
   ]) 
   const [newName, setNewName] = useState('')
-
-  const addName = (event) => {
+  const [newNumber, setNewNumber] = useState('')
+  
+  const addContact = (event) => {
     event.preventDefault()
     
     if (persons.some(person => person.name === newName)) {
@@ -18,27 +20,40 @@ const App = () => {
     } else {
       const contactObject = {
         name: newName,
+        number: newNumber,
         id: persons.length + 1,
       }
       setPersons(persons.concat(contactObject))
       setNewName('')
+      setNewNumber('')
     }
   }
 
-  const handleInputChange = (event) => {
-    console.log(event.target.value)
-    setNewName(event.target.value)
+  const handleNameChange = (event) => {
+    console.log(event.target)
+      setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    console.log(event.target)
+      setNewNumber(event.target.value)
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
         <div>
-          <form onSubmit={addName}>
-            name: <input
+          <form onSubmit={addContact}>
+            name: <input title='name' id='1'
               value={newName}
-              onChange={handleInputChange}
+              onChange={handleNameChange}
             />
+            <br></br>
+            number: <input title='number' id='2'
+              value={newNumber}
+              onChange={handleNumberChange}
+            />
+            <br></br>
             <button type="submit">add</button>
           </form>
         </div>
