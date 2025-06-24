ec266fa1caf66ab7e05d5ba2d95b61fe6301ec40
VehvilainenPooki <pooki.vehvilainen@helsinki.fi>
2025-06-24 15:58:05 +0300

    Ex 3.1

--- Parts/part 3/Phonebook and Backend/src/components/Person.jsx
+++
@@ -1,8 +0,0 @@
-const Person = ({ person, removeEvent }) => {
-    return (
-      <p>{person.name} {person.number} <button value={person.id} onClick={removeEvent}>delete</button></p>
-    )
-  }
-  
-  export default Person
-  
