9debd9ee2a2a09cc400bf9ff5571ecd75522a815
VehvilainenPooki <pooki.vehvilainen@helsinki.fi>
2025-07-19 15:07:07 +0300

    Ex 3.20

--- Parts/part_3/Phonebook_and_Backend/models/person.js
+++ Parts/part_3/Phonebook_and_Backend/models/person.js
@@ -8,12 +8,12 @@ const url = process.env.MONGODB_URI
 
 console.log('connecting to', url)
 mongoose.connect(url)
-    .then(result => {
-        console.log('connected to MongoDB')
-    })
-    .catch(error => {
-        console.log('error connecting to MongoDB:', error.message)
-    })
+.then(result => {
+    console.log('connected to MongoDB')
+})
+.catch(error => {
+    console.log('error connecting to MongoDB:', error.message)
+})
 */
 
 const personSchema = new mongoose.Schema({
@@ -26,6 +26,12 @@ const personSchema = new mongoose.Schema({
     number: {
         type: String,
         minLength: 8,
+        validate:  {
+            validator: function(v) {
+                return /^\d{2,3}-\d{6,10}$/.test(v);
+            },
+            message: props => `${props.value} is not a valid phone number! Phone number must be 2-3 numbers divided by a dash (-) and 6-10 numbers.`
+        },
         required: true
     },
 })
