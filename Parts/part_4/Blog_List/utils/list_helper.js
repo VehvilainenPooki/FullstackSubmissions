307ddec35b1e6debfb61c7e3220c26412393932c
VehvilainenPooki <pooki.vehvilainen@helsinki.fi>
2025-08-05 20:01:42 +0300

    Ex 4.4

--- Parts/part_4/Blog_List/utils/list_helper.js
+++ Parts/part_4/Blog_List/utils/list_helper.js
@@ -1,7 +1,17 @@
+
 const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+    likesSum = 0
+    blogs.forEach(blog => {
+        likesSum = likesSum + blog.likes
+    })
+    return likesSum
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
