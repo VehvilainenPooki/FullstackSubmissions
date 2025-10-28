2e6e2e0fbe87838e1e3c7b483da9a7bc9fdbe3f7
VehvilainenPooki <pooki.vehvilainen@helsinki.fi>
2025-10-28 21:47:49 +0200

    Ex 5.7

--- Parts/part_5/Blog_List/bloglist-frontend/src/components/Blog.jsx
+++ Parts/part_5/Blog_List/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,37 @@
-const Blog = ({ blog }) => (
-    <div>
-        {blog.title} {blog.author}
-    </div>
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+    const [visible, setVisible] = useState(false)
+
+    const toggleVisibility = () => {
+        setVisible(!visible)
+    }
+
+    const blogStyle = {
+        paddingTop: 10,
+        paddingLeft: 2,
+        border: 'solid',
+        borderWidth: 1,
+        marginBottom: 5
+    }
+
+    return (
+        <div style={blogStyle}>
+            {blog.title} {blog.author}
+            {!visible && <button onClick={toggleVisibility}>view</button>}
+            {visible && <button onClick={toggleVisibility}>hide</button>}
+
+            {visible && (
+                <div>
+                    {blog.url}
+                    <br/>
+                    {blog.likes} <button>like</button>
+                    <br/>
+                    {blog.user.name}
+                </div>
+            )}
+        </div>
+    )
+}
 
 export default Blog
