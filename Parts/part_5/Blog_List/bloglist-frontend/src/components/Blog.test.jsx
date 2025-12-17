e5ecee037545b9835c54a874489a8b108ca511bf
VehvilainenPooki <pooki.vehvilainen@helsinki.fi>
2025-12-17 17:06:00 +0200

    5.14.

--- Parts/part_5/Blog_List/bloglist-frontend/src/components/Blog.test.jsx
+++ Parts/part_5/Blog_List/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,25 +1,43 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
-test('Blog renders by default', () => {
-    const blog = {
-        title: 'Stringer',
-        author: 'Linger',
-        url: 'StringerLinger.com',
-        likes: 13,
-        user: {
-            username: 'calcato',
-            name: 'calcato Maganado',
-            id: '68b044f56cf63b0f040480db'
+describe('<Blog />', () => {
+    beforeEach(() => {
+        const blog = {
+            title: 'Stringer',
+            author: 'Linger',
+            url: 'StringerLinger.com',
+            likes: 13,
+            user: {
+                username: 'calcato',
+                name: 'calcato Maganado',
+                id: '68b044f56cf63b0f040480db'
+            }
         }
-    }
 
-    render(<Blog blog={blog} />)
+        render(<Blog blog={blog}/>)
+    })
 
-    const blogText = screen.getByText('Stringer Linger')
-    const hiddenText = screen.queryByText('StringerLinger.com')
-    const hiddenLikes = screen.queryByText(13)
-    expect(blogText).toBeDefined()
-    expect(hiddenText).toBeNull()
-    expect(hiddenLikes).toBeNull()
+    test('Blog renders by default', () => {
+        const blogText = screen.getByText('Stringer Linger')
+        const hiddenUrl = screen.queryByText('StringerLinger.com')
+        const hiddenLikes = screen.queryByText(13)
+        expect(blogText).toBeDefined()
+        expect(hiddenUrl).toBeNull()
+        expect(hiddenLikes).toBeNull()
+    })
+
+    test('Blog renders stuff behind button', async () => {
+        const user = userEvent.setup()
+        const toggleButton = screen.getByText('view')
+        await user.click(toggleButton)
+
+        const blogText = screen.getByText('Stringer Linger')
+        const hiddenUrl = screen.findByText('StringerLinger.com')
+        const hiddenLikes = screen.findByText(13)
+        expect(blogText).toBeDefined()
+        expect(hiddenUrl).toBeDefined()
+        expect(hiddenLikes).toBeDefined()
+    })
 })
