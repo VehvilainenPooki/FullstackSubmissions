182509de455e4c1f7fb7c4ac94088a0afe6f074f
VehvilainenPooki <pooki.vehvilainen@helsinki.fi>
2025-12-20 11:12:05 +0200

    5.15.

--- Parts/part_5/Blog_List/bloglist-frontend/src/components/Blog.test.jsx
+++ Parts/part_5/Blog_List/bloglist-frontend/src/components/Blog.test.jsx
@@ -2,6 +2,8 @@ import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
+var handleLike = vi.fn()
+
 describe('<Blog />', () => {
     beforeEach(() => {
         const blog = {
@@ -15,8 +17,8 @@ describe('<Blog />', () => {
                 id: '68b044f56cf63b0f040480db'
             }
         }
-
-        render(<Blog blog={blog}/>)
+        handleLike = vi.fn()
+        render(<Blog blog={blog} handleLike={handleLike}/>)
     })
 
     test('Blog renders by default', () => {
@@ -39,5 +41,17 @@ describe('<Blog />', () => {
         expect(blogText).toBeDefined()
         expect(hiddenUrl).toBeDefined()
         expect(hiddenLikes).toBeDefined()
+    })
+
+    test('Blog like button works', async () => {
+        const user = userEvent.setup()
+        const toggleButton = screen.getByText('view')
+        await user.click(toggleButton)
+        const likeButton = screen.getByText('like')
+        expect(handleLike.mock.calls).toHaveLength(0)
+        await user.click(likeButton)
+        expect(handleLike.mock.calls).toHaveLength(1)
+        await user.click(likeButton)
+        expect(handleLike.mock.calls).toHaveLength(2)
     })
 })
