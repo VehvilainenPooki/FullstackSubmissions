d755174895a3b5cac55825fd9201f06dc8d9a08a
VehvilainenPooki <pooki.vehvilainen@helsinki.fi>
2026-01-31 12:41:33 +0200

    Ex 6.14.-15.

--- Parts/part_6/redux-anecdotes/src/components/AnecdoteForm.jsx
+++ Parts/part_6/redux-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,18 +1,19 @@
 import { useDispatch, useSelector } from 'react-redux'
 import { createAnecdote } from '../reducers/anecdoteReducer'
 import { setNotification } from '../reducers/notificationReducer'
+import anecdoteService from '../services/anecdotes'
 
 const AnecdoteForm = () => {
   const dispatch = useDispatch()
   const anecdotes = useSelector(state => state.anecdotes)
 
 
-  const addAnecdote = (event) => {
+  const addAnecdote = async (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
-    console.log(content)
-    dispatch(createAnecdote(content))
+    const newAnecdote = await anecdoteService.createNew(content)
+    dispatch(createAnecdote(newAnecdote))
     dispatch(setNotification(`New anecdote added: ${content}`))
   }
 
--- Parts/part_6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ Parts/part_6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,38 +1,23 @@
 import { createSlice } from '@reduxjs/toolkit'
 
-const anecdotesAtStart = [
-  ['If it hurts, do it more often', 0, 0],
-  ['Adding manpower to a late software project makes it later!', 1, 1],
-  ['The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.', 2, 2],
-  ['Any fool can write code that a computer can understand. Good programmers write code that humans can understand.', 3, 3],
-  ['Premature optimization is the root of all evil.', 4, 4],
-  ['Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.', 5, 5]
-]
-
-const asObject = anecdote => {
-  return {
-    content: anecdote[0],
-    id: anecdote[1],
-    votes: anecdote[2]
-  }
-}
-
-const initialState = anecdotesAtStart.map(asObject)
 const anecdoteSlice = createSlice({
   name: 'anecdotes',
-  initialState,
+  initialState: [],
   reducers : {
     createAnecdote(state, action) {
-      state.push({content: action.payload, id: state.length, votes: 0})
+      state.push(action.payload)
     },
     vote(state, action) {
       const id = action.payload
       const anecdoteVote = state.find(anec => anec.id === id)
       const changedAnecdote = {content: anecdoteVote.content, id: anecdoteVote.id, votes: (anecdoteVote.votes + 1)}
       return state.map(anec => (anec.id !== id ? anec : changedAnecdote))
+    },
+    setAnecdotes(state, action) {
+      return action.payload
     }
   }
 })
 
-export const {createAnecdote, vote} = anecdoteSlice.actions
+export const { createAnecdote, vote, setAnecdotes } = anecdoteSlice.actions
 export default anecdoteSlice.reducer
